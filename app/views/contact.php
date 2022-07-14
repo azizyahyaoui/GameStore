81cbe086b59c9925851d7748319e2a96fdeecfdc
Yahyaoui Med Aziz <azizyahyaoui716@gmail.com>
2022-07-14 23:34:17 +0100

    add home,contact and app main layout

---
+++ app/views/contact.php
@@ -0,0 +1,20 @@
+<h1>contact us</h1>
+<form action="" method="post">
+    <div class="mb-3">
+        <label>Email address</label>
+        <input type="email" name="email" class="form-control" id="exampleInputEmail1">
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Subject</label>
+        <input type="text"  name="Subject"class="form-control" id="exampleInputPassword1">
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Body</label>
+        <textarea name="body"  cols="30" rows="10" class="form-control"></textarea>
+    </div>
+    <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
